cbd9408fcc1c03ced6c675039f5c59e567495e1c
Norawit Nangsue <norawitn@hotmail.com>
2021-03-29 21:31:41 +0700

    update and change to boost

--- src/rmdtcp_driver_node.cpp
+++ src/rmdtcp_driver_node.cpp
@@ -1,16 +1,18 @@
 #include <rmdtcp_driver/rmdtcp_driver_hw.h>
 #include <controller_manager/controller_manager.h>
+#include <ros/ros.h>
 
 int main()
 {
-  rmdtcp_driver_hardware_interface::RMDTCP_Driver driver;
+  rmdtcp_driver_hardware_interface::RMDTCP_Driver driver("192.168.16.21", "9001");
   controller_manager::ControllerManager cm(&driver);
+
+  
 
   while (true)
   {
-    const ros::Duration period(1.0);
-     driver.read();
-     cm.update(driver.get_time(), driver.get_period());
+     driver.read(
+     cm.update(ros::Time::now(), );
      driver.write();
   }
 }
